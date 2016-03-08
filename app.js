bd54917756ac32beac8259069a7ae140858287c1
hpcfapix <hpcfapix@hlrs.de>
2016-03-08 11:53:10 +0100

    change of elastic host to EXCESS cluster

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var os = require("os");
 var elasticsearch = require('elasticsearch');
 var elastic = new elasticsearch.Client({
-  host: 'localhost:9200',
+  host: '192.168.0.160:9200',
   log: 'error'
 });
 
