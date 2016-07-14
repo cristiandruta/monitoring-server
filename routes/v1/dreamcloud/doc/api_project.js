77e9965e6490df2355a62e05453cd41dc74172f2
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-07-14 11:45:19 +0200

    include first batch of dreamcloud related api documentation

---
+++ routes/v1/dreamcloud/doc/api_project.js
@@ -0,0 +1,26 @@
+define({
+  "name": "monitoring-server",
+  "version": "1.0.0",
+  "description": "RESTful services for the ATOM monitoring framework",
+  "title": "DreamCloud RESTful API",
+  "url": "http://mf.excess-project.eu:3030/v1/dreamcloud/mf",
+  "header": {
+    "title": "DreamCloud API"
+  },
+  "order": [
+    "GetWorkflows",
+    "PostWorkflows"
+  ],
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-07-14T09:43:40.227Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
